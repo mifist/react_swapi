759f0b8a6d1cdda3f1cf152e3234f6fcbcf69935
Mifist <daria1992moskalets@gmail.com>
2021-05-07 18:15:41 +0300

    added simple navigation

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Header from 'components/Header';
 import Footer from 'components/Footer';
 // custom image
 import sw_logo from 'assets/images/Star_Wars_Yellow_Logo.svg';
+import {LogoYellow} from 'assets/images/cusom-icons';
 
 const App = () => {
   return (
@@ -15,12 +16,7 @@ const App = () => {
         <div className='container'>
           <div className='row'>
             <div className='col-md-6 ml-auto mr-auto'>
-              <p>
-                An artist of considerable range, Chet Faker &#x2014; the name
-                taken by Melbourne-raised, Brooklyn-based Nick Murphy &#x2014;
-                writes, performs and records all of his own music, giving it a
-                warm, intimate feel with a solid groove structure.{' '}
-              </p>
+              <LogoYellow />
             </div>
           </div>
         </div>
