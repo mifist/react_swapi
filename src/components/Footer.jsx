f7782e0612becfdbdb25ca8ec1e0b568ffb5d478
Mifist <daria1992moskalets@gmail.com>
2021-05-12 17:23:54 +0300

    added copyright ti the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,16 +7,23 @@ import {Heart} from '@styled-icons/icomoon/Heart';
 export const HeartIcon = styled(Heart)`
   width: 16px;
   margin: 0 7px;
+  color: #FFE81F;
 `;
 
 function Footer({createdBy}) {
   return (
-    <footer id='sw-app-footer' className='footer footer-default'>
+    <footer id='sw-app-footer' className='footer footer-default' style={{flexWrap: 'wrap'}}>
       <div
         className='copyright float-right'
-        style={{display: 'flex', alignItems: 'center'}}
+        style={{display: 'flex', alignItems: 'center', justifyContent: 'flex-end'}}
       >
-        &copy; 2021, made with <HeartIcon /> by {createdBy} for a better web.
+        &copy; 2021, designed and developed with <HeartIcon /> by {createdBy} for a better web.
+      </div>
+      <div className='copyright' style={{display: 'block', width: '100%', fontSize: '12px', paddingTop: '35px'}}>
+        Star Wars and all associated names and/or images are copyright Lucasfilm
+        Ltd. Images were freely collected from{' '}
+        <a href='http://starwars.wikia.com/wiki/Main_Page' rel="noreferrer" target='_blank'>Wookiepedia</a>.<br />
+        Designed by <a href='https://www.creative-tim.com/product/material-kit' rel="noreferrer" target='_blank'>Material Kit</a>
       </div>
     </footer>
   );
